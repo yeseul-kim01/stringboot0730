162995a345af93bc64161c91943709a8aa9438be
yeseul-kim01 <yesul0718@naver.com>
2024-07-30 09:51:00 +0900

    work

---
+++ src/main/java/org/example/stringboot0730/Stringboot0730Application.java
@@ -0,0 +1,13 @@
+package org.example.stringboot0730;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stringboot0730Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Stringboot0730Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stringboot0730/Stringboot0730ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stringboot0730;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stringboot0730ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
